0fed21b711508d1e362b2f2058777c238e8db9ea
Alex <alexrodriguezserrano1595@gmail.com>
2019-01-29 12:27:09 -0600

    Modifico handler de error para mayor control

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,9 +5,21 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use App\Traits\ApiResponse;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Illuminate\Auth\Access\AuthorizationException;
+
+
+
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponse;
     /**
      * A list of the exception types that should not be reported.
      *
@@ -44,6 +56,49 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof ValidationException){
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+
+        if($exception instanceof ModelNotFoundException){
+            $model = strtolower(basename($exception->getModel()));
+            return $this->errorResponse("No exite el modelo {$model}", 404);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $exception);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return $this->errorResponse("No tiene permisos para ejecutar esta accion", 403);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse("EndPoint no encontrado", 404);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse("El metodo no es valido", 405);
+        }
+
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(),$exception->getStatusCode());
+        }
+
+        if ($exception instanceof QueryException) {
+            $codigo = $exception->errorInfo[1];
+            if($codigo == 1451)
+                return $this->errorResponse('No se puede eliminar, recurso con relaciones',409);
+        }
+
+        /*if ($exception instanceof QueryException) {
+            return $this->errorResponse("Error en base de datos",409);
+        }*/
+
+        if(config('api.debug')){
+            return $this->errorResponse('Error inesperado. Intenta mas tarde',500);
+        }
+
         return parent::render($request, $exception);
     }
 
@@ -56,10 +111,30 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
+        return $this->errorResponse('No auntenticado', 401);
+
+//        if ($request->expectsJson()) {
+//            return response()->json(['error' => 'Unauthenticated.'], 401);
+//        }
+//
+//        return redirect()->guest(route('login'));
+    }
+
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        if ($e->response) {
+            return $e->response;
         }
 
-        return redirect()->guest(route('login'));
+        $errors = $e->validator->errors()->getMessages();
+
+        return $this->errorResponse($errors, 422);
     }
 }
